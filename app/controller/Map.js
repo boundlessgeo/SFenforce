cb313f9062495a190bbee685037014ec469ce8b0
Bart van den Eijnden <bartvde@opengeo.org>
2012-05-30 10:52:36 +0200

    only update last refresh when data gets in

--- app/controller/Map.js
+++ app/controller/Map.js
@@ -16,15 +16,18 @@ Ext.define('SFenforce.controller.Map', {
 
     },
 
+    doUpdate: function() {
+        this.getLastRefresh().setHtml('Last refresh: ' + Ext.Date.format(new Date(), 'H:i A'));
+        var map = this.getMap().getMap();
+        var layer = map.getLayersByClass('OpenLayers.Layer.Vector')[0];
+        layer.events.un({'loadend': this.doUpdate, scope: this});
+    },
+
     doRefresh: function() {
         var map = this.getMap().getMap();
-        for (var i=0, ii=map.layers.length; i<ii; ++i) {
-            var layer = map.layers[i];
-            if (layer instanceof OpenLayers.Layer.Vector) {
-                layer.refresh({force: true});
-            }
-        }
-        this.getLastRefresh().setHtml('Last refresh: ' + Ext.Date.format(new Date(), 'H:i A'));
+        var layer = map.getLayersByClass('OpenLayers.Layer.Vector')[0];
+        layer.events.on({'loadend': this.doUpdate, scope: this});
+        layer.refresh({force: true});
     }
 
 });
