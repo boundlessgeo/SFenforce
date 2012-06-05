8a452dd985d810305ab9e66908eef61d3f9557df
Bart van den Eijnden <bartvde@opengeo.org>
2012-06-05 16:18:53 +0200

    the previous change broke the vector layer in the map, restored now

--- app/util/Config.js
+++ app/util/Config.js
@@ -9,49 +9,7 @@ Ext.define('SFenforce.util.Config', {
         featureNS: 'http://www.sfpark.org/SFenforce',
         noDataLayerName: "No Data Spaces",
         citationLayerName: "Citation opportunities",
-        style: new OpenLayers.Style({
-                pointRadius: 6,
-                fillOpacity: 0.85,
-                graphicName: 'circle'
-            }, {
-                rules: [
-                    new OpenLayers.Rule({
-                        name: 'Unpaid vehicle',
-                        filter: new OpenLayers.Filter.Comparison({
-                            type: OpenLayers.Filter.Comparison.EQUAL_TO,
-                            property: 'METER_EXPIRED_FLAG',
-                            value: 1
-                        }),
-                        symbolizer: {
-                            fillColor: '#FF0000'
-                        }
-                    }),
-                    new OpenLayers.Rule({
-                        name: 'Vehicle at commercial space',
-                        filter: new OpenLayers.Filter.Comparison({
-                            type: OpenLayers.Filter.Comparison.EQUAL_TO,
-                            property: 'COMMERCIAL_OCCUPIED_FLAG',
-                            value: 1
-                        }),
-                        symbolizer: {
-                            fillColor: '#EFEF20'
-                        }
-                    }),
-                    new OpenLayers.Rule({
-                        name: 'Cited',
-                        filter: new OpenLayers.Filter.Comparison({
-                            type: OpenLayers.Filter.Comparison.EQUAL_TO,
-                            property: 'DISPOSITION_CODE',
-                            value: 1
-                        }),
-                        symbolizer: {
-                            fillColor: '#22FF11',
-                            pointRadius: 4
-                        }
-                    })
-                ]
-            }
-        )
+        style: null
     },
 
     constructor: function(config) {
