5d6b7cdcf37317c5857cb88e72deb58093cc0d51
Bart van den Eijnden <bartvde@opengeo.org>
2012-09-05 13:14:13 +0200

    textual change

--- app/util/Config.js
+++ app/util/Config.js
@@ -147,7 +147,7 @@ Ext.define('SFenforce.util.Config', {
         doneButtonText: "DONE",
         errorTitle: "Sorry, but an error occurred.",
         transactionErrorText: "An error occurred - please try saving again.",
-        transactionSuccessText: "Submitted.",
+        transactionSuccessText: "Data submitted.",
         gpsErrorMsg: "GPS information is currently unavailable. Please try again when GPS is available, or enable GPS for this application on your device.",
         /** end i18n */
 
