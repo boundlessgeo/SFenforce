7238a1a5a70097f043c346982b7c5c1fb97e43af
Bart van den Eijnden <bartvde@opengeo.org>
2012-09-21 10:37:41 +0200

    add SQL for updating disposition code values

---
+++ data/disposition_update.sql
@@ -0,0 +1,14 @@
+delete from disposition_codes_tmp;
+insert into disposition_codes_tmp values(1, 'Citation issued');
+insert into disposition_codes_tmp values(2, 'Disabled placard');
+insert into disposition_codes_tmp values(3, 'City placard');
+insert into disposition_codes_tmp values(4, 'Other permit');
+insert into disposition_codes_tmp values(5, 'Meter paid');
+insert into disposition_codes_tmp values(6, 'Space empty');
+insert into disposition_codes_tmp values(7, 'Space closed');
+insert into disposition_codes_tmp values(8, 'Broken meter');
+insert into disposition_codes_tmp values(9, 'Commercial vehicle');
+insert into disposition_codes_tmp values(10, 'Vehicle attended');
+insert into disposition_codes_tmp values(11, 'Vehicle already visited');
+insert into disposition_codes_tmp values(12, 'Other');
+commit;
